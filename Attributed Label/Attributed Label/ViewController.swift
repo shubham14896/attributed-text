2ff3af386ff9e0fa2a13b4dd75fc18e62e442eeb
shubham14896 <shubham9032@gmail.com>
2018-06-06 16:44:08 +0530

    Bug Fix

--- Attributed Label/Attributed Label/ViewController.swift
+++ Attributed Label/Attributed Label/ViewController.swift
@@ -22,9 +22,9 @@ class ViewController: UIViewController {
         
         label1.text = "Hi, I am normal label text."
         
-        let attributedText = NSMutableAttributedString(string: "Hello I am", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17)])
-        attributedText.append(NSAttributedString(string: " Attributed Text ", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 17), NSAttributedStringKey.foregroundColor: UIColor.blue]))
-        attributedText.append(NSMutableAttributedString(string: ", assigned to label.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17)]))
+        let attributedText = NSMutableAttributedString(string: "Hello, I am", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17)])
+        attributedText.append(NSAttributedString(string: " Attributed Text,", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 17), NSAttributedStringKey.foregroundColor: UIColor.blue]))
+        attributedText.append(NSMutableAttributedString(string: " assigned to label.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17)]))
         
         label2.attributedText = attributedText
         
